a95867938b1adb9817dc0d6574cc654c1e47cd1c
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-09-27 17:22:47 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -311,16 +311,18 @@ async def echo(bot, update):
                     update.chat.id
                 )
         await fmsg.delete()
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
-            reply_markup=reply_markup,
-            parse_mode="html",
-            reply_to_message_id=update.message_id
-        )
+        message_text = Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD
+       # await bot.send_message(
+          #  chat_id=update.chat.id,
+          #  text=Translation.FORMAT_SELECTION.format(thumbnail) + "\n" + Translation.SET_CUSTOM_USERNAME_PASSWORD,
+          #  reply_markup=reply_markup,
+         #   parse_mode="html",
+         #   reply_to_message_id=update.message_id
+       # )
     else:
-        # fallback for nonnumeric port a.k.a seedbox.io
-        inline_keyboard = []
+        # ---------fallback for nonnumeric port a.k.a seedbox.io######
+        
+        #inline_keyboard = []
         cb_string_file = "{}={}={}".format(
             "file", "LFO", "NONE")
         cb_string_video = "{}={}={}".format(
@@ -335,15 +337,24 @@ async def echo(bot, update):
                 callback_data=(cb_string_file).encode("UTF-8")
             )
         ])
+        message_text = Translation.FORMAT_SELECTION.format("")
+        inline_keyboard.append([InlineKeyboardButton("✖️ CLOSE ✖️",callback_data="close")])
         reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
+        
         await fmsg.delete()
-        await bot.send_message(
-            chat_id=update.chat.id,
-            text=Translation.FORMAT_SELECTION.format(""),
+        await update.reply_text(
+            text=message_text,
             reply_markup=reply_markup,
             parse_mode="html",
-            reply_to_message_id=update.message_id
+            quote=True
         )
+       # await bot.send_message(
+           # chat_id=update.chat.id,
+          #  text=Translation.FORMAT_SELECTION.format(""),
+           # reply_markup=reply_markup,
+         #   parse_mode="html",
+           # reply_to_message_id=update.message_id
+      #  )
 ###############################
 async def notify(update, time_gap):
     await asyncio.sleep(30)
