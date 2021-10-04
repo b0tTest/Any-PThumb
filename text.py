201337bc3da1b18b23a8fd186537bea7c75e18bb
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-04 17:14:51 +0530

    Create text.py

---
+++ text.py
@@ -0,0 +1,8 @@
+class Text(object):
+    START_TEXT = """<b>Hai {} </b>,
+a simple Telegram URL Upload Bot!
+it can <b>UPLOAD almost all Direct Links to Telegram as File/Video</b>
+ 
+🚨 Dont Upload PORN video🔞 Links you will Get PERMANENT BAN 🚨
+┈┈┈••💙✿❤✿💚••┈┈┈
+"""
