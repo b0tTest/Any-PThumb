e7ec0af8f535b415e8995a0360d47ecb43f7fab2
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-04 16:11:25 +0530

    emoji

--- plugins/dl_button.py
+++ plugins/dl_button.py
@@ -262,8 +262,10 @@ async def download_coroutine(bot, session, url, file_name, chat_id, message_id, 
             chat_id,
             message_id,
             text="""Initiating Download
-URL: {}
-File Size: {}""".format(url, humanbytes(total_length))
+            
+🔗URL: {}
+
+📁File Size: {}""".format(url, humanbytes(total_length))
         )
         with open(file_name, "wb") as f_handle:
             while True:
@@ -283,11 +285,12 @@ File Size: {}""".format(url, humanbytes(total_length))
                     estimated_total_time = elapsed_time + time_to_completion
                     try:
                         current_message = """**Download Status**
-URL: {}
-File Size: {}
-Downloaded: {}
-Speed : {}
-ETA: {}""".format(
+🔗URL: {}
+
+**📁 File Size.......: {}
+📥 Downloaded.: {}
+🐌 Speed.......... : {}
+⏱ Time Left.....: {}**""".format(
     url,
     humanbytes(total_length),
     humanbytes(downloaded),
