7b984c02603404d752116905247bebe67eb1ced3
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-05 14:49:57 +0530

    Update text.py

--- text.py
+++ text.py
@@ -31,6 +31,8 @@ it can <b>UPLOAD almost all Direct Links to Telegram as File/Video</b>
    
   <b> Thats it, I will Do Rest of it 😌</b>
 
+➠ <a href="https://t.me/myTestbotz/73"> How to Download Gdrive files </a>
+➠ <a href="https://t.me/MyTestBotZ/349"> Fix Thumbnail issues (temporary) </a>
 """
     
     ABOUT_TEXT = """Hi {},
