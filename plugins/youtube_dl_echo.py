b95c1bccd33d4bbdaf60503692e68b6ae24438fc
Esther-Lopez <66007532+Esther-Lopez@users.noreply.github.com>
2021-10-05 13:15:28 +0530

    Update youtube_dl_echo.py

--- plugins/youtube_dl_echo.py
+++ plugins/youtube_dl_echo.py
@@ -56,7 +56,7 @@ async def echo(bot, update):
             )
         except Exception as error:
             print(error)
-    logger.info(update.from_user.id)
+    #logger.info(update.from_user.id)
     fmsg = await update.reply_text(text=Translation.CHECKING_LINK, quote=True)
     url = update.text
     if Config.UPDATE_CHANNEL:
